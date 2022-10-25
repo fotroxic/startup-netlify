06cc21c25195e35142a8f057b975f4414bc08dc9
Onur Ago <fotroxic@gmail.com>
2022-10-25 16:41:55 +0200

    pls'

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -20,7 +20,7 @@ export default function Blog({ frontmatter, markdown}) {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const fileContent = matter(fs.readFileSync(`./content/blogs/${slug}.md`, 'utf8'))
+  const fileContent = matter(fs.readFileSync(`../../content/blogs/${slug}.md`, 'utf8'))
   let frontmatter = fileContent.data
   const markdown = fileContent.content
 
@@ -30,7 +30,7 @@ export async function getStaticProps({ params: { slug } }) {
 }
 
 export async function getStaticPaths() {
-  const filesInProjects = fs.readdirSync('./content/blogs')
+  const filesInProjects = fs.readdirSync('../../content/blogs')
 
   // Getting the filenames excluding .md extension
   // and returning an array containing slug (the filename) as params for every route
